e674d178e8bd40dd0a126bd6be52cbd43236afa2
Dylan McReynolds <dmcreynolds@lbl.gov>
2025-02-11 10:58:28 -0800

    checkpoint

--- src/arroyogisaxs/lse/lse_operator.py
+++ src/arroyogisaxs/lse/lse_operator.py
@@ -5,10 +5,9 @@ import logging
 import os
 from typing import Type, TypeVar
 
-# import joblib
+import joblib
 import torch
-
-# import torchvision.transforms as transforms
+import torchvision.transforms as transforms
 from arroyopy.operator import Operator
 
 from ..config import settings
@@ -25,7 +24,83 @@ RESULTS_TILED_API_KEY = os.getenv("RESULTS_TILED_API_KEY", None)
 
 logger = logging.getLogger(__name__)
 
-T = TypeVar("T", bound="LatentSpaceOperator")
+T = TypeVar("T", bound="Reducer")
+
+
+class Reducer:
+    def __init__(self):
+        self.current_torch_model = None
+        self.curent_dim_reduction_model = None
+        self.current_transform = None
+        self.loaded_torch_models = {}  # cache models as they're accessed
+        self.loaded_dim_reduction_models = {}  # cache models as they're accessed
+        # Check for CUDA else use CPU
+        # needs to be members of the reducer class
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+            logger.info(f"Using CUDA: {torch.cuda.get_device_name(0)}")
+        else:
+            device = torch.device("cpu")
+            logger.info("Using CPU")
+        self.device = device
+
+    def extract_ls(self, message: GISAXSRawEvent):
+        # 1. Encode the image into a latent space
+        tensor = self.current_transform(
+            message.image.array
+        )  # Add batch and channel dimensions
+        f_vec_nn = self.current_torch_model.module.encoder(tensor)
+        # 2. Dimension Reduction (PCA)
+        f_vec = self.curent_dim_reduction_model.transform(f_vec_nn)
+        return f_vec
+
+    def get_torch_model(self):
+        current_config = settings.models.torch.current
+        for model_config in settings.models.torch.models:
+            if model_config.name == current_config:
+                current_model = model_config
+                break
+        if model_config is None:
+            raise ValueError(f"Current model {current_config} is not found in models")
+        if current_model.name in self.loaded_torch_models:
+            return self.loaded_torch_models[current_model]
+        else:
+            model = load_torch_model(current_model)
+            model = model.to(self.device)
+            self.loaded_torch_models[current_model] = model
+            return model
+
+    def get_dim_reduction_model(self):
+        current_model = settings.models.dim_reduction.current
+        if current_model in self.loaded_dim_reduction_models:
+            return self.loaded_dim_reduction_models[current_model]
+        else:
+            dim_reduction_model = joblib.load(settings.models.dim_reduction_model.dir)
+            self.loaded_dim_reduction_models[current_model] = dim_reduction_model
+            return dim_reduction_model
+
+    def get_transform(self):
+        return transforms.Compose(
+            [
+                transforms.Resize(
+                    (128, 128)
+                ),  # Resize to smaller dimensions to save memory
+                transforms.ToTensor(),  # Convert image to PyTorch tensor (0-1 range)
+                transforms.Normalize(
+                    (0.0,), (1.0,)
+                ),  # Normalize tensor to have mean 0 and std 1
+            ]
+        )
+
+    @classmethod
+    def from_settings(cls: Type[T]) -> T:
+        reducer = cls()
+        # Load the models now
+        reducer.current_torch_model = reducer.get_torch_model()
+        reducer.curent_dim_reduction_model = reducer.get_dim_reduction_model()
+        reducer.current_transform = reducer.get_transform()
+
+        return reducer
 
 
 class LatentSpaceOperator(Operator):
@@ -38,44 +113,9 @@ class LatentSpaceOperator(Operator):
     agorithm to a 2D space. The results are saved to a Tiled dataset.
     """
 
-    def __init__(self, model: torch.nn, dim_reduction_model, transform):
+    def __init__(self, reducer: Reducer):
         super().__init__()
-
-    @classmethod
-    def from_settings(cls: Type[T]) -> T:
-        module_name, class_name = settings.models.torch_model.python_class.rsplit(
-            ".", 1
-        )
-        module = importlib.import_module(module_name)
-        model_class = getattr(module, class_name)
-        model = model_class()
-        checkpoint = torch.load(settings.models.torch_model.checkpoint_dir)
-        model.load_state_dict(checkpoint["model_state_dict"], strict=False)
-        # dim_reduction_model = joblib.load(settings.models.dim_reduction_model.dir)
-
-        # Check for CUDA else use CPU
-        if torch.cuda.is_available():
-            device = torch.device("cuda")
-            logger.info(f"Using CUDA: {torch.cuda.get_device_name(0)}")
-        else:
-            device = torch.device("cpu")
-            logger.info("Using CPU")
-        model = model.to(device)
-
-        # Define the transformation to apply to the data
-        # transform = transforms.Compose(
-        #     [
-        #         transforms.Resize(
-        #             (128, 128)
-        #         ),  # Resize to smaller dimensions to save memory
-        #         transforms.ToTensor(),  # Convert image to PyTorch tensor (0-1 range)
-        #         transforms.Normalize(
-        #             (0.0,), (1.0,)
-        #         ),  # Normalize tensor to have mean 0 and std 1
-        #     ]
-        # )
-        # operator = cls(model, dim_reduction_model, transform)
-        # return operator
+        self.reducer = reducer
 
     async def process(self, message: GISAXSMessage) -> None:
         if isinstance(message, GISAXSRawStart):
@@ -89,13 +129,15 @@ class LatentSpaceOperator(Operator):
             logger.warning(f"Unknown message type: {type(message)}")
         return None
 
-    def extract_ls(self, message: GISAXSRawEvent):
-        # 1. Encode the image into a latent space
-        tensor = self.transform(message.image.array)  # Add batch and channel dimensions
-        f_vec_nn = self.model.module.encoder(tensor)
-        # 2. Dimension Reduction (PCA)
-        f_vec = self.dim_reduction_model.transform(f_vec_nn)
-        return f_vec
+
+def load_torch_model(model_config: dict) -> torch.nn.Module:
+    module_name, class_name = model_config.python_class.rsplit(".", 1)
+    module = importlib.import_module(module_name)
+    model_class = getattr(module, class_name)
+    model = model_class()
+    checkpoint = torch.load(settings.models.torch.model.checkpoint_dir)
+    model.load_state_dict(checkpoint["model_state_dict"], strict=False)
+    return model
 
 
 def import_torch_models():
@@ -115,6 +157,7 @@ def import_torch_models():
     return torch_models
 
 
-def main():
-    operator = LatentSpaceOperator.from_settings()
+if __name__ == "__main__":
+    reducer = Reducer.from_settings()
+    operator = LatentSpaceOperator(reducer)
     operator.run()
--- src/arroyogisaxs/one_d_reduction/operator.py
+++ src/arroyogisaxs/one_d_reduction/operator.py
@@ -1,11 +1,9 @@
 import logging
 
-import numpy as np
-import pandas as pd
 from arroyopy.operator import Operator
-from arroyopy.schemas import DataFrameModel
 
 from ..schemas import GISAXS1DReduction, GISAXSRawEvent, GISAXSRawStart, GISAXSRawStop
+from .reduce import pixel_roi_horizontal_cut
 
 logger = logging.getLogger(__name__)
 
@@ -13,33 +11,24 @@ logger = logging.getLogger(__name__)
 class OneDReductionOperator(Operator):
     def __init__(self):
         super().__init__()
-        self.dummy1d = Dummy1D()
-        self.current_run_url = None
-        self.show_msg = True
+        self.current_reduction_settings = None
 
     async def process(self, message):
         if isinstance(message, GISAXSRawStart):
-            self.current_run_url = message.tiled_url
-            self.show_msg = True
             logger.info(f"Processing Start {message}")
+            self.current_reduction_settings = get_reduction_settings()
+            logger.info("")
             await self.publish(message)
 
         if isinstance(message, GISAXSRawStop):
-            logger.info(f"Processing Stio {message}")
+            logger.info(f"Processing Stop {message}")
             await self.publish(message)
 
         if isinstance(message, GISAXSRawEvent):
-            curve_df = self.dummy1d.next_curve()
-            if self.current_run_url is None:
-                if self.show_msg:
-                    logger.info(
-                        "It looks like this was started out of order. Waiting for start message."
-                    )
-                    self.show_msg = False
-                return
-            curve = DataFrameModel(df=curve_df)
+            # For now
+            reduction = pixel_roi_horizontal_cut(**self.current_reduction_settings)
             reduction_msg = GISAXS1DReduction(
-                curve=curve,
+                curve=reduction,
                 curve_tiled_url=self.current_run_url,
                 raw_frame=message.image,
                 raw_frame_tiled_url=self.current_run_url,
@@ -47,14 +36,5 @@ class OneDReductionOperator(Operator):
             await self.publish(reduction_msg)
 
 
-class Dummy1D:
-    def __init__(self) -> pd.DataFrame:
-        self.angle = 0
-
-    def next_curve(self):
-        if self.angle == 180:
-            self.angle = 0
-        wave = np.sin(np.linspace(self.angle, np.pi, 180))
-        self.angle += 1
-        df = pd.DataFrame([wave])
-        return df
+def get_reduction_settings() -> dict:
+    return dict()
