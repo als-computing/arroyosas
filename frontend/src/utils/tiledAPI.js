8a005272ef16ff3ef6e4da6de7fec4564784cccd
seijdeleon <seijdeleon@lbl.gov>
2025-02-20 12:28:28 -0500

    change tiled api calls to fetch and set force cache

--- frontend/src/utils/tiledAPI.js
+++ frontend/src/utils/tiledAPI.js
@@ -9,10 +9,19 @@ export const getTiled = () => {
 export const getSearchResults = async (searchPath, cb, mock = false) => {
     try {
         const sampleTiledUrl = "http://127.0.0.1:8000/api/v1/array/full/exp01/ML_exp01-144J-22_id836920_?slice=158,::1,::1"
-        const response = await axios.get(searchPath);
+/*         const response = await axios.get(searchPath);
         //console.log({response});
         //cb(response.data);
-        return response.data;
+        return response.data; */
+        const response = await fetch(searchPath, {
+            method: "GET",
+            cache: "force-cache", // Ensures the request uses cached data if available
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        return await response.json();
     } catch (error) {
         console.error('Error searching path: ', error);
     }
