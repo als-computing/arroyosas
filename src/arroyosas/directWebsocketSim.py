c815f76a703a12d9267578a5e0cf5e50d83d21fb
seijdeleon <seijdeleon@lbl.gov>
2025-07-17 14:53:06 -0700

    send out 2d array for curve in sim websocket

--- src/arroyosas/directWebsocketSim.py
+++ src/arroyosas/directWebsocketSim.py
@@ -1,20 +1,74 @@
-#pip install -e .
-#cd src/
+# How do I run this the first time? Install everything and then run it as a module from the project ROOT
+# pip install -e .
+# python -m arroyosas.directWebsocketSim
 import asyncio
 import json
 import logging
 from typing import Union
 from glob import glob
+import os
 
 from PIL import Image
 import msgpack
 import numpy as np
 import websockets
 from arroyopy.publisher import Publisher
+from .one_d_reduction.reduce import pixel_roi_horizontal_cut
+#from .one_d_reduction.operator import OneDReductionOperator #the methods needed in this file are now instance, not static
 
-from .schemas import RawFrameEvent, SASStart, SASStop, SerializableNumpyArrayModel
+#copied from one_d_reduction.operator.py, add as static method in future refactor 
+def generate_masked_image(image, mask):
+    masked_float = mask.astype(float)  
+    masked_float[masked_float == True] = np.nan  #At true values set to NaN
+    masked_float[masked_float == 0] = 1 #At false values set to 1
+    masked_image = image * masked_float #Multiply to set masked values to NaN
+    return masked_image
+
+#copied from one_d_reduction.operator.py, add as static method in future refactor 
+def load_static_mask_file():
+    try:
+        # assumed path from project root is masks/mask.npy 
+        project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+        mask_path = os.path.join(project_root, "masks", "mask.npy")
+        mask = np.load(mask_path)
+        logger.info(f"Mask loaded successfully from {mask_path}, shape: {mask.shape}")
+        return mask
+
+    except FileNotFoundError:
+        logger.error(f"Mask file not found at: {mask_path}")
+    except Exception as e:
+        logger.error(f"Error loading mask file: {e}")
+    return None
+
+
+from .schemas import  RawFrameEvent, SASStart, SASStop, SerializableNumpyArrayModel
 
 logger = logging.getLogger(__name__)
+
+mask = load_static_mask_file()
+logger.info(f"Mask shape: {mask.shape if mask is not None else 'None'}")
+#TO DO: handle conditions where detector image is 'flipped' so y values should be from (top - input_height)
+parameters_smi = {
+    "masked_image": None,
+    "beamcenter_x": 251,
+    "beamcenter_y": 200,
+    "incident_angle": 0.15,
+    "sample_detector_dist": 100,
+    "wavelength": 1.2398,
+    "pix_size": 172,
+    "cut_half_width": 1,
+    "cut_pos_y": 90,
+    "x_min": 0,
+    "x_max": 500,
+    "output_unit": "q"
+}
+
+linecut = {
+    "cut_half_width": parameters_smi["cut_half_width"],
+    "cut_pos_y": parameters_smi["cut_pos_y"],
+    "x_min": parameters_smi["x_min"],
+    "x_max": parameters_smi["x_max"],
+}
 
 class OneDWSPublisher(Publisher):
     """
@@ -30,6 +84,7 @@ class OneDWSPublisher(Publisher):
         super().__init__()
         self.host = host
         self.port = port
+
 
     async def start(
         self,
@@ -119,14 +174,20 @@ def pack_images(message: RawFrameEvent) -> bytes:
     Pack all the images into a single msgpack message
     """
     try:
+        masked_image = generate_masked_image(message.image.array, mask)
+        parameters_smi["masked_image"] = masked_image
+        q_coordinates, reduction, _ = pixel_roi_horizontal_cut(**parameters_smi)
+        plot_array = np.column_stack((q_coordinates, reduction))
+        serializable_reduction = SerializableNumpyArrayModel(array=plot_array)
         return msgpack.packb(
             {
                 "raw_frame": convert_to_uint8(message.image.array),
-                #"curve": message.curve.df.to_json(),
+                "curve": serializable_reduction.array.tobytes(),
                 "raw_frame_tiled_url": message.tiled_url,
                 "curve_tiled_url": message.tiled_url,
                 "width": message.image.array.shape[1],
                 "height": message.image.array.shape[0],
+                "linecut": linecut
                 #"data_type": message.image.array.dtype,
             }
         )
@@ -142,12 +203,13 @@ async def test_client(publisher: OneDWSPublisher, num_frames: int = 10):
     from arroyopy.schemas import DataFrameModel, NumpyArrayModel
 
     from arroyosas.schemas import (
-       # GISAXSImageInfo,
         RawFrameEvent,
         SASStart,
         SASStop,
         SerializableNumpyArrayModel
     )
+
+
 
     await asyncio.sleep(2)
     for y in range(100):
@@ -159,31 +221,15 @@ async def test_client(publisher: OneDWSPublisher, num_frames: int = 10):
             data_type="float32",
             tiled_url="http://example.com/tiled"
         ))
-        # for x in range(num_frames):
-        #     await asyncio.sleep(1)
-        #     # Create a test pattern image that changes slightly each time
-        #     frame_number = int(time.time()) % 100  # Change pattern every second
-
-
-        #files = glob("/Users/seij/SMI_Experiments/exp01/*.tif")
+       
         files = glob("/Users/seij/SMI_Experiments/feb/*.tiff")
-        #path = "/Users/seij/SMI_Experiments/feb/"
-        #for f in os.listdir(path):
-         #   print(f"{repr(f)}")
-
         print(f"Total files found: {len(files)}")
         frame_num = 0
         for file in files:
             with Image.open(file) as img:
-                #print(f"Image mode: {img.mode}")  # 'L', 'RGB', 'RGBA', or multi-band
                 img = img.convert("L")  #Force greyscale
                 arr = np.array(img)  # 
-                #arr_normalized = (arr - arr.min()) / (arr.max() - arr.min()) * 255
-                #arr_normalized = arr_normalized.astype(np.uint8)  # Convert to uint8
-                #print(f"Loaded image {file}, shape: {arr.shape}, dtype: {arr.dtype}")
-                #link ="http://127.0.0.1:8000/api/v1/array/full/exp01/ML_exp01-144J-22_id836920_?slice=" + str(frame_num) + ",::1,::1"
                 link ="http://127.0.0.1:8000/api/v1/array/full/feb/pil1M_image?slice=" + str(frame_num)
-
 
                 event = RawFrameEvent(
                     image=SerializableNumpyArrayModel(array=arr),
@@ -193,33 +239,6 @@ async def test_client(publisher: OneDWSPublisher, num_frames: int = 10):
                 print("event")
                 await publisher.publish(event)
             await asyncio.sleep(5)
-
-            # #Creates a sample image 
-            # image = np.zeros((100, 100), dtype=np.float32)
-            # np.fill_diagonal(image, frame_number % 255)
-
-            # # Create a 1D sine wave pattern
-            # x = np.linspace(0, 2 * np.pi, 100)
-            # one_d_reduction = pd.DataFrame(
-            #     {"q": x, "qy": np.sin(x + frame_number * 0.1)}
-            # )
-            # image_info = {
-            #     "frame_number": frame_number,
-            #     "width": image.shape[1],
-            #     "height": image.shape[0],
-            #     "data_type": "uint8",
-            # }
-
-            # # Create GISAXSResult message
-            # message = GISAXSRawEvent(
-            #     #image_info=GISAXSImageInfo(**image_info),
-            #     image=SerializableNumpyArrayModel(array=image),
-            #     one_d_reduction=DataFrameModel(df=one_d_reduction),
-            #     frame_number=frame_number,
-            #     tiled_url="fake_url"
-            # )
-
-            #await publisher.publish(message)
             frame_num = frame_num + 1
         await publisher.publish(SASStop(num_frames=num_frames))
 
